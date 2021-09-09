de807c166617dbd41a8e7b7e742e8e705a209bc6
Reagan <88494643+CaseyReagan@users.noreply.github.com>
2021-09-09 23:44:02 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,9 @@ int main(void)
 
 	printf("Hello,World!\n");
 	printf("铎哥说commit不够了就来这里写一条printf.\n")；
-	printf("铎哥真聪明。");
+	printf("铎哥真聪明。\n");
+	
+	printf("今天9月9号，昨天push的太harder了，有点累，所以今日代码量不够，又来摸鱼。\n");
+	printf("因为提到了push harder，便跟铎哥讨论为什么没有github娘");
 	return 0;
 }
